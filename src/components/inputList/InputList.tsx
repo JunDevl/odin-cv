e941cc9434195942c8294d8e2c963632647ad341
JunDevl <juninhoplay.pedone@gmail.com>
2026-01-17 21:43:55 -0300

    Started implementation of input list's height calculation based on how many items were added to the list

--- src/components/inputList/InputList.tsx
+++ src/components/inputList/InputList.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react"
 import "./inputlist.css"
+import { useRef } from "react";
 
 interface Props extends React.HTMLProps<HTMLInputElement> {
   itemsState: { knownFrameworks: string[], setKnownFrameworks: React.Dispatch<React.SetStateAction<string[]>> }
@@ -7,10 +7,9 @@ interface Props extends React.HTMLProps<HTMLInputElement> {
 const InputList = ({itemsState, ...props}: Props) => {
   const INSERT_ENTRY_TEXT = "+ Press enter to insert a new entry";
 
-  const {knownFrameworks, setKnownFrameworks} = itemsState;
+  const frameworksListElement = useRef<HTMLUListElement>(null);
 
-  type ListVisibility = "shown" | "hidden"
-  const [ listVisibility, setListVisibility ] = useState<ListVisibility>("hidden");
+  const {knownFrameworks, setKnownFrameworks} = itemsState;
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const target = e.target as HTMLInputElement;
@@ -18,24 +17,45 @@ const InputList = ({itemsState, ...props}: Props) => {
     if (target.value !== "" && 
         target.value !== INSERT_ENTRY_TEXT &&
         e.key === "Enter" && 
-        !knownFrameworks.includes(target.value)) 
+        !knownFrameworks.includes(target.value)) {
       setKnownFrameworks([...knownFrameworks, target.value])
-    
+      
+    }
+  }
+
+  const handleFocus = (e: React.FocusEvent<HTMLDivElement>) => {
+    console.log(e);
+    frameworksListElement.current!.style.height = `calc(var(--input-height)*${knownFrameworks.length + 2})`;
+  }
+
+  const handleBlur = (e: React.FocusEvent<HTMLDivElement>) => {
+    console.log(e);
+    frameworksListElement.current!.style.height = "100%";
   }
 
   return (
-    <div className="input-list">
+    <div 
+      className="input-list"
+      // tabIndex={-1} wip
+    >
       <label htmlFor={props.name}>Known languages/frameworks:</label>
-      <input 
-        {...props} 
-        onFocus={() => setListVisibility("shown")} 
-        onBlur={() => setListVisibility("hidden")}
-        onKeyDown={(e) => handleKeyDown(e)}
-      />
-      <ul className={`list ${listVisibility}`}>
-        {knownFrameworks.map(item => <li key={item}>{item}</li>)}
-        <li key={INSERT_ENTRY_TEXT} className="insert-entry">{INSERT_ENTRY_TEXT}</li>
-      </ul>
+      <div className="input-wrapper">
+        <input 
+          {...props} 
+          onKeyDown={(e) => handleKeyDown(e)}
+          onFocus={(e) => handleFocus(e)}
+          onBlur={(e) => handleBlur(e)}
+        />
+        <ul className={`frameworks-list`} ref={frameworksListElement}>
+          <li key={INSERT_ENTRY_TEXT} className="insert-entry">{INSERT_ENTRY_TEXT}</li>
+          {knownFrameworks.map(item => (
+            <li className="framework-item" key={item}>
+
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
